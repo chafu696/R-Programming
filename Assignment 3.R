40ac4ed62a451728b85a55e54e29a08911673062
MBailey93 <70748922+MBailey93@users.noreply.github.com>
2020-09-04 11:45:59 +0200

    Lab 3-1

---
+++ Assignment 3.R
@@ -0,0 +1,17 @@
+euclidean <- function(x,y) {
+  if(is.numeric(x) & is.numeric(y) & length(x)==1 & length(y)==1 & x%%1==0 & y%%1==0){
+    while(x != y) {
+      if (x>y) {
+       x <- (x-y)
+      }
+      else {
+       y <- (y-x)
+      }
+   }
+   return(x)
+  }
+  else{
+    stop("Arguments must be two numeric scalars")
+  }
+}
+
